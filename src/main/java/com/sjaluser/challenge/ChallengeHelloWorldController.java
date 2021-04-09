b7946bb045b4d0c6741cc561d13159c7d8060175
Javier Aransay <aransayj@gmail.com>
2021-04-09 14:28:45 +0200

    Log action

--- src/main/java/com/sjaluser/challenge/ChallengeHelloWorldController.java
+++ src/main/java/com/sjaluser/challenge/ChallengeHelloWorldController.java
@@ -1,9 +1,14 @@
 package com.sjaluser.challenge;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
@@ -11,9 +16,13 @@ class ChallengeHelloWorldController {
  @Autowired
  private ChallengeHelloWorld challengeHelloWorld;
 
- 	 @CrossOrigin
+ 	Logger logger = LoggerFactory.getLogger(ChallengeHelloWorldController.class);
+ 	
+	 @CrossOrigin
 	 @RequestMapping("/hello")
 	 public String hello(@RequestParam("navigator") String navigator) {
-		 return challengeHelloWorld.hello(navigator);
+
+        logger.info(String.format("Hello World request, navigator %s", navigator));
+		return challengeHelloWorld.hello(navigator);
  	}
 }
